bc2a2d75d15b934085da0abf4de52494231659d4
Sneha <63293229+snehan316@users.noreply.github.com>
2022-02-08 01:54:25 +0530

    Initial Commit

---
+++ AMQConsumer/src/main/java/com/demo/HelloWorldQueueMDB.java
@@ -0,0 +1,38 @@
+package com.demo;
+
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import org.jboss.ejb3.annotation.ResourceAdapter;
+
+
+@MessageDriven(name = "HelloWorldQ", activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "HelloWorldQ"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+@ResourceAdapter(value="activemq-rar-5.16.3.rar")
+public class HelloWorldQueueMDB implements MessageListener {
+
+    private final static Logger LOGGER = Logger.getLogger(HelloWorldQueueMDB.class.toString());
+
+    /**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message rcvMessage) {
+        TextMessage msg = null;
+        try {
+            if (rcvMessage instanceof TextMessage) {
+                msg = (TextMessage) rcvMessage;
+                LOGGER.info("Received Message from queue: " + msg.getText());
+            } else {
+                LOGGER.warning("Message of wrong type: " + rcvMessage.getClass().getName());
+            }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
